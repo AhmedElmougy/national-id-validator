3def5719075773c1df6612ebeb2def4e01d338b3
AhmedElmougy <ahmed.m.elmogy@gmail.com>
2021-02-27 05:54:03 +0200

    create national id model

--- national_id/admin.py
+++ national_id/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from national_id.models import NationalId
+
+
 # Register your models here.
+admin.site.register(NationalId)
--- national_id/models.py
+++ national_id/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class NationalId(models.Model):
+    """
+    Egyption National Id model representation
+    """
+
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    national_id = models.IntegerField(unique=True)
+    date_of_birth = models.DateField(null=True)
+    place_of_birth = models.CharField(max_length=16, default='')
+    gender = models.CharField(max_length=1, choices=GENDER, default='')
+
+    def __str__(self):
+
+        return str(self.national_id)
